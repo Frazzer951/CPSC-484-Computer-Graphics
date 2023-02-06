aca174f0c8f2f53f0379b6a44c6bb941fccf40d9
Frazzer951 <luke343279@gmail.com>
2023-02-06 12:30:23 -0800

    reflections

--- python/RayTracingFromScratch/ray_tracer.py
+++ python/RayTracingFromScratch/ray_tracer.py
@@ -7,6 +7,10 @@ def normalize(vector):
     return vector / np.linalg.norm(vector)
 
 
+def reflected(vector, axis):
+    return vector - 2 * np.dot(vector, axis) * axis
+
+
 def sphere_intersect(center, radius, ray_origin, ray_direction):
     b = 2 * np.dot(ray_direction, ray_origin - center)
     c = np.linalg.norm(ray_origin - center) ** 2 - radius**2
@@ -33,9 +37,18 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
 width = 300
 height = 200
 
+max_depth = 3
+
 camera = np.array([0, 0, 1])
 ratio = float(width) / height
 screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
+
+light = {
+    "position": np.array([5, 5, 5]),
+    "ambient": np.array([1, 1, 1]),
+    "diffuse": np.array([1, 1, 1]),
+    "specular": np.array([1, 1, 1]),
+}
 
 objects = [
     {
@@ -45,6 +58,7 @@ objects = [
         "diffuse": np.array([0.7, 0, 0]),
         "specular": np.array([1, 1, 1]),
         "shininess": 100,
+        "reflection": 0.5,
     },
     {
         "center": np.array([0.1, -0.3, 0]),
@@ -53,6 +67,7 @@ objects = [
         "diffuse": np.array([0.7, 0, 0.7]),
         "specular": np.array([1, 1, 1]),
         "shininess": 100,
+        "reflection": 0.5,
     },
     {
         "center": np.array([-0.3, 0, 0]),
@@ -61,65 +76,74 @@ objects = [
         "diffuse": np.array([0, 0.6, 0]),
         "specular": np.array([1, 1, 1]),
         "shininess": 100,
+        "reflection": 0.5,
+    },
+    {
+        "center": np.array([0, -9000, 0]),
+        "radius": 9000 - 0.7,
+        "ambient": np.array([0.1, 0.1, 0.1]),
+        "diffuse": np.array([0.6, 0.6, 0.6]),
+        "specular": np.array([1, 1, 1]),
+        "shininess": 100,
+        "reflection": 0.5,
     },
 ]
-light = {
-    "position": np.array([5, 5, 5]),
-    "ambient": np.array([1, 1, 1]),
-    "diffuse": np.array([1, 1, 1]),
-    "specular": np.array([1, 1, 1]),
-}
 
 image = np.zeros((height, width, 3))
 with alive_bar(width * height) as bar:
     for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
         for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
+            # screen is on origin
             pixel = np.array([x, y, 0])
             origin = camera
             direction = normalize(pixel - origin)
 
-            # Check for intersections
-            nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
-            if nearest_object is None:
-                bar()
-                continue
+            color = np.zeros((3))
+            reflection = 1
 
-            intersection = origin + min_distance * direction
+            for k in range(max_depth):
+                # check for intersections
+                nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
+                if nearest_object is None:
+                    break
 
-            normal_to_surface = normalize(intersection - nearest_object["center"])
-            shifted_point = intersection + 1e-5 * normal_to_surface
-            intersection_to_light = normalize(light["position"] - shifted_point)
+                intersection = origin + min_distance * direction
+                normal_to_surface = normalize(intersection - nearest_object["center"])
+                shifted_point = intersection + 1e-5 * normal_to_surface
+                intersection_to_light = normalize(light["position"] - shifted_point)
 
-            _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
-            intersection_to_light_distance = np.linalg.norm(light["position"] - intersection)
-            is_shadowed = min_distance < intersection_to_light_distance
+                _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
+                intersection_to_light_distance = np.linalg.norm(light["position"] - intersection)
+                is_shadowed = min_distance < intersection_to_light_distance
 
-            if is_shadowed:
-                bar()
-                continue
+                if is_shadowed:
+                    break
 
-            # RGB
-            illumination = np.zeros((3))
+                illumination = np.zeros((3))
 
-            # ambiant
-            illumination += nearest_object["ambient"] * light["ambient"]
+                # ambiant
+                illumination += nearest_object["ambient"] * light["ambient"]
 
-            # diffuse
-            illumination += nearest_object["diffuse"] * light["diffuse"] * np.dot(intersection_to_light, normal_to_surface)
+                # diffuse
+                illumination += nearest_object["diffuse"] * light["diffuse"] * np.dot(intersection_to_light, normal_to_surface)
 
-            # specular
-            intersection_to_camera = normalize(camera - intersection)
-            H = normalize(intersection_to_light + intersection_to_camera)
-            illumination += (
-                nearest_object["specular"]
-                * light["specular"]
-                * np.dot(normal_to_surface, H) ** (nearest_object["shininess"] / 4)
-            )
+                # specular
+                intersection_to_camera = normalize(camera - intersection)
+                H = normalize(intersection_to_light + intersection_to_camera)
+                illumination += (
+                    nearest_object["specular"]
+                    * light["specular"]
+                    * np.dot(normal_to_surface, H) ** (nearest_object["shininess"] / 4)
+                )
 
-            image[i, j] = np.clip(illumination, 0, 1)
+                # reflection
+                color += reflection * illumination
+                reflection *= nearest_object["reflection"]
 
-            # image[i, j] = ...
+                origin = shifted_point
+                direction = reflected(direction, normal_to_surface)
 
+            image[i, j] = np.clip(color, 0, 1)
             bar()
 
 
