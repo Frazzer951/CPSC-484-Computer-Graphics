8eeebe7c33decf535f60af3ec431890beac7a9b4
Luke Eltiste <luke343279@gmail.com>
2023-03-06 09:50:36 -0800

    format

--- qt/cube/main.cpp
+++ qt/cube/main.cpp
@@ -53,25 +53,24 @@
 #include <QSurfaceFormat>
 
 #ifndef QT_NO_OPENGL
-#include "mainwidget.h"
+#  include "mainwidget.h"
 #endif
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+int main( int argc, char *argv[] ) {
+  QApplication app( argc, argv );
 
-    QSurfaceFormat format;
-    format.setDepthBufferSize(24);
-    QSurfaceFormat::setDefaultFormat(format);
+  QSurfaceFormat format;
+  format.setDepthBufferSize( 24 );
+  QSurfaceFormat::setDefaultFormat( format );
 
-    app.setApplicationName("cube");
-    app.setApplicationVersion("0.1");
+  app.setApplicationName( "cube" );
+  app.setApplicationVersion( "0.1" );
 #ifndef QT_NO_OPENGL
-    MainWidget widget;
-    widget.show();
+  MainWidget widget;
+  widget.show();
 #else
-    QLabel note("OpenGL Support required");
-    note.show();
+  QLabel note( "OpenGL Support required" );
+  note.show();
 #endif
-    return app.exec();
+  return app.exec();
 }
